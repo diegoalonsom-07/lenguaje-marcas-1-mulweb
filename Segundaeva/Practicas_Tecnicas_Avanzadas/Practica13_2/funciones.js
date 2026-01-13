131160f7d855e41281f49e84356ffbf0ba4f078b
Diego Alonso <diegoalonsomartinez2007@gmail.com>
2026-01-13 19:47:18 +0100

    "13-01-2026 19:47"

---
+++ Segundaeva/Practicas_Tecnicas_Avanzadas/Practica13_2/funciones.js
@@ -0,0 +1,10 @@
+function resaltado(id_objeto) {
+    document.getElementById(id_objeto).style.height = "300px";
+    document.getElementById(id_objeto).style.width = "400px";
+    document.getElementById(id_objeto).style.opacity = "100%";
+}
+function normal(id_objeto) {
+    document.getElementById(id_objeto).style.height = "150px";
+    document.getElementById(id_objeto).style.width = "200px";
+    document.getElementById(id_objeto).style.opacity = "50%";
+}
