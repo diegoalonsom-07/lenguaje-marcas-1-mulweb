ab03fbaf7b1713f710aec2f4de88efbca98c624e
Diego Alonso <diegoalonsomartinez2007@gmail.com>
2026-03-03 19:42:32 +0100

    "03-03-2026 19:42"

---
+++ Segundaeva/Examen_Diego_Alonso/Parte_HTML/Scripts/funciones.js
@@ -0,0 +1,24 @@
+function borde_rojo(id_objeto) {
+    document.getElementById(id_objeto).style.border = "6px solid red";
+    document.getElementById(id_objeto).style.outline = "none"; 
+}
+
+function borde_normal(id_objeto) {
+    document.getElementById(id_objeto).style.border = "";
+}
+function oscurecer(id_objeto) {
+    document.getElementById(id_objeto).style.backgroundColor = "#d1d1d1";
+}
+function aclarar(id_objeto) {
+    document.getElementById(id_objeto).style.backgroundColor = "";
+}
+function avisarEnvio() {
+    alert("Se van a enviar todos los datos del formulario");
+}
+function confirmarBorrado() {
+    var respuesta = confirm("¿No quieres aprobar?");
+    return respuesta; 
+}
+function mostrarFecha() {
+    alert(new Date().toLocaleDateString());
+}
